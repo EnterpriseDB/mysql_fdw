043a77c9b4a600f0884b4e50ee05be68c3c731e7
Rodrigo Menezes <r@seedfi.com>
2019-11-18 17:30:14 -0500

    Add support for binary / varbinary

--- mysql_fdw.c
+++ mysql_fdw.c
@@ -1946,6 +1946,8 @@ mysqlImportForeignSchema(ImportForeignSchemaStmt *stmt, Oid serverOid)
                      "    WHEN c.DATA_TYPE = 'blob' THEN 'bytea'"
                      "    WHEN c.DATA_TYPE = 'mediumblob' THEN 'bytea'"
                      "    WHEN c.DATA_TYPE = 'longblob' THEN 'bytea'"
+                     "    WHEN c.DATA_TYPE = 'binary' THEN 'bytea'" 
+                     "    WHEN c.DATA_TYPE = 'varbinary' THEN 'bytea'" 
                      "    ELSE c.DATA_TYPE"
                      "  END,"
                      "  c.COLUMN_TYPE,"
